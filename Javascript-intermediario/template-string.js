de36ad4e14ed589855862730ea0fc3f4cef7b65a
Patricia Fonseca <patricia.fonseca.sf@gmail.com>
2021-08-09 21:48:14 -0300

    Aula Javascript Básico

---
+++ Javascript-intermediario/template-string.js
@@ -0,0 +1,15 @@
+let meuNome = 'Patricia';
+let meuSobrenome = 'Fonseca';
+let minhaProfissao = 'Estudante';
+
+/* console.log(
+  'Olá eu sou ' +
+    meuNome +
+    ' ' +
+    meuSobrenome +
+    ' minha profissão é: ' +
+    minhaProfissao
+); */
+console.log(
+  `Olá, meu nome é ${meuNome} ${meuSobrenome} e minha profissão é: ${minhaProfissao}`
+);
---
+++ Javascript-intermediario/ternario.js
@@ -0,0 +1,9 @@
+let isvalid = true;
+
+function verify(isvalid) {
+  if (isvalid) {
+    return true;
+  } else {
+    return false;
+  }
+}
